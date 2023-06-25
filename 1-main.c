be33e4f47b1f6f2a46ab700ff0c8f2c343e9c0af
Bucha1958 <okoriemmadu@gmail.com>
2023-06-25 01:24:00 -0700

    selection sort

--- 1-main.c
+++ 1-main.c
@@ -1,58 +1,56 @@
-#include <stddef.h>
-#include "sort.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "sort.h"
 
 /**
+ * create_listint - Creates a doubly linked list from an array of integers
  *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
  *
- *
- *
+ * Return: Pointer to the first element of the created list. NULL on failure
  */
-listint_t *add_new_list(listint_t **head, int n)
+listint_t *create_listint(const int *array, size_t size)
 {
-  listint_t *new, *temp;
-  
-  temp = *head;
-  if (new == NULL)
-    return NULL;
-  new = malloc(sizeof(listint_t));
-  new->n = n;
-  new->next = NULL;
-  if (*head == NULL)
-  {
-    *head = new;
-    return (new);
-  }
-  while (temp->next != NULL)
-  {
-      temp = temp->next;
-  }
-  temp->next = new;
-  temp = new;
-  return new;
+    listint_t *list;
+    listint_t *node;
+    int *tmp;
+
+    list = NULL;
+    while (size--)
+    {
+        node = malloc(sizeof(*node));
+        if (!node)
+            return (NULL);
+        tmp = (int *)&node->n;
+        *tmp = array[size];
+        node->next = list;
+        node->prev = NULL;
+        list = node;
+        if (list->next)
+            list->next->prev = list;
+    }
+    return (list);
 }
 
 /**
+ * main - Entry point
  *
- *
- *
+ * Return: Always 0
  */
 int main(void)
 {
-  listint_t *head;
+    listint_t *list;
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
 
-  head = NULL;
-  add_new_list(&head, 0);
-  add_new_list(&head, 1);
-  add_new_list(&head, 5);
-  add_new_list(&head, 8);
-  add_new_list(&head, 9);
-  add_new_list(&head, 4);
-  add_new_list(&head, 3);
-  add_new_list(&head, 14);
-  print_list(head);
-  return 0;
+    list = create_listint(array, n);
+    if (!list)
+        return (1);
+    print_list(list);
+    printf("\n");
+    insertion_sort_list(&list);
+    printf("\n");
+    print_list(list);
+    return (0);
 }
-  
-  
